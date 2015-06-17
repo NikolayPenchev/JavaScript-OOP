459272a832f6041dc44aa7dbf18f673b26f1d22f
Nikolay Penchev <nikolay.pen4ev@gmail.com>
2015-06-17 11:22:47 +0300

    Update task-1.js

--- 01. Functions and Function Expressions/00. homework/tasks/task-1.js
+++ 01. Functions and Function Expressions/00. homework/tasks/task-1.js
@@ -8,8 +8,22 @@
 
 */
 
-function sum() {
-	
+function sum(arr) {
+	if(arr.length===0){
+		return null;
+	}
+	var i,
+		len=arr.length,
+		number = 0,
+		sum = 0;
+	for (var i = 0; i < len; i++) {
+		if (isNaN(arr[i])) { // check if not a number trow ERROR
+			throw 'Number must be of type Number';
+		}else {
+			sum += +arr[i];
+		}
+	}
+	return sum;
 }
 
 module.exports = sum;
